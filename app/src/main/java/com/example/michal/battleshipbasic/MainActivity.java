f54ea4cd850e5142374891066eb13bcf6eb161dc
michalk933 <6pak.tv@gmail.com>
2016-09-02 17:30:57 +0200

    :)

--- app/src/main/java/com/example/michal/battleshipbasic/MainActivity.java
+++ app/src/main/java/com/example/michal/battleshipbasic/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.michal.battleshipbasic;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+        /// start ;)
     }
 
     @Override
